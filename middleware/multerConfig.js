e4ab5342f68e29362a31be7fd172883bca606367
Birat <thapabirat95@gmail.com>
2025-05-01 10:51:51 +0545

    bug fix

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -10,7 +10,6 @@ const multer =require('multer')
 // })
 
 const storage = multer.diskStorage({
-
     filename : function(req,file,cb){
        cb(null, Date.now() + "-" + file.originalname)
     }
